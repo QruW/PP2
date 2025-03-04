bf47e02d5217df77a714c6840731464d23caf44f
QruW <aibaryerbol@gmail.com>
2025-03-05 02:26:56 +0500

    Labs

--- Labs/lab_6.4.py
+++ Labs/lab_6.4.py
@@ -1,13 +1,3 @@
-import os
+filename = "test.txt"
 
-path = r'C:\Users\aibar\OneDrive\Рабочий стол\Вообщем и в целом\VSCode\Новая папка'
-
-if os.access(path, os.F_OK):
-    contents = os.listdir(path)
-    path_1 = './'
-    for element in contents:
-        print(element)
-        print(path_1)
-else:
-    print("This path is not exist")
-    
+print(sum(1 for _ in open (filename, 'r')))

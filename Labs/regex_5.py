adfc298cf758fa0fa65950dc7b07379cd1ec854e
QruW <aibaryerbol@gmail.com>
2025-03-05 01:44:09 +0500

    Labs

---
+++ Labs/regex_5.py
@@ -0,0 +1,9 @@
+import re
+
+with open('test.txt', 'r') as file:
+    content = file.read()
+
+def match_a_anything_b(s):
+    return re.fullmatch(r'a.*b', s) is not None
+
+print(match_a_anything_b(content))
